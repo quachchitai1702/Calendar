4880811e1342589e2ab3a75d704aa5c16b29f55f
Tài Quách Chí <22521285@ms.uit.edu.vn>
2023-12-23 21:00:41 +0700

    Update

--- ViewModel/CalendarVM.cs
+++ ViewModel/CalendarVM.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calendar.Model;
 
 namespace Calendar.ViewModel
 {
-    class CalendarVM
+    class CalendarVM : Utilities.ViewModelBase
     {
+        private readonly PageModel _pageModel;
+
+        public CalendarVM()
+        {
+            _pageModel = new PageModel();
+        }
     }
 }
